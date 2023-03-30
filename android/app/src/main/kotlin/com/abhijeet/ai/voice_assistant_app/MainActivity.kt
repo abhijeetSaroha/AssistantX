0614a5bca54d4097a620b0fc1dbb601b7286130d
abhijeetSaroha <abhijeetsaroha08@gmail.com>
2023-03-31 00:48:41 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/abhijeet/ai/voice_assistant_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abhijeet.ai.voice_assistant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = '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';
